11f01e06c3b4f481971a6d34423a50ecf89bcd77
1gory <igor.pershin.me@gmail.com>
2019-10-10 16:55:44 +0300

    fix scroll

--- src/index.jsx
+++ src/index.jsx
@@ -2,15 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import '@babel/polyfill';
 import 'normalize.css';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Provider } from 'unstated';
 import './assets/style/reset.css';
 import './assets/style/fonts.css';
 import App from './App';
 
+const ScrollToTop = () => {
+  window.scrollTo(0, 0);
+  return null;
+};
+
 ReactDOM.render(
   <Router>
     <Provider>
+      <Route component={ScrollToTop} />
       <App />
     </Provider>
   </Router>,
